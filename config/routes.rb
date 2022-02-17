2fc9a501315d222a7cc10b11e39d85944eff8d8a
shota1719 <shota.shirai@prum.jp>
2022-02-17 20:35:50 +0900

    loginscreen

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'top#index'
+  get 'auth/login', to: 'auth#login'
   get 'fares/car-ferry', to: 'fares#car_ferry'
   get 'fares/jetfoil', to: 'fares#jetfoil'
+  root to: "auth#login"
 end
