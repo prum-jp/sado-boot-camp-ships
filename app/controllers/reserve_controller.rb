eedf91d02c0d073aadf81be02e1e5ad5b926e9b5
hideki.ishibashi <hideki.ishibashi@prum.jp>
2022-03-11 11:41:17 +0900

    del: unused files

--- app/controllers/reserve_controller.rb
+++
@@ -1,6 +0,0 @@
-class ReserveController < ApplicationController
-    def index
-      render :index
-    end
-  end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,5 @@ Rails.application.routes.draw do
   get '/', to: 'top#index'
   get 'fares/car-ferry', to: 'fares#car_ferry'
   get 'fares/jetfoil', to: 'fares#jetfoil'
-  get 'reserve/index', to: 'reserve#index'
   get 'reservation', to: 'reservation#index'
 end
